ba67a50bdf2eb1d44723b2a508cc2682fc0e9f78
Soumya <111266105+soumyasen10@users.noreply.github.com>
2024-05-29 00:20:22 +0530

    Create ASCII.cpp

---
+++ ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    char ch; 
+  
+    ch = 'A'; 
+  
+    cout << "The ASCII value of " << ch << " is " << int(ch) 
+         << endl; 
+  
+    return 0; 
+}
