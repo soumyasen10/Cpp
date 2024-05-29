572d64be42cd0992c9fc0f87006af1bb63bbabb0
Soumya <111266105+soumyasen10@users.noreply.github.com>
2024-05-30 00:59:36 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// function to find factorial 
+int factorial(int n) 
+{ 
+    int fact = 1; 
+    while (n > 1) { 
+        fact *= n; 
+        n--; 
+    } 
+  
+    return fact; 
+} 
+  
+// driver code 
+int main() 
+{ 
+    int num = 5; 
+  
+    cout << factorial(num); 
+  
+    return 0; 
+}
