88dcdff5f10b3c261dcf2ca72b9d26c5eb235ae3
Soumya <111266105+soumyasen10@users.noreply.github.com>
2024-06-03 23:05:20 +0530

    Create MouseProgramming.cpp

---
+++ MouseProgramming.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <graphics.h>
+
+int main()
+{
+
+    int gdriver = DETECT, gmode, errorcode;
+
+    initgraph(&gdriver, &gmode, "C:\\TC\\BGI");
+ 
+
+    errorcode = graphresult();
+ 
+
+    if (errorcode == grOk)
+
+        cout << "Graphics enabled: \n"
+
+             << grapherrormsg(errorcode);
+
+    else
+
+        cout << "Graphics error: \n"
+
+             << grapherrormsg(errorcode);
+ 
+
+    getch();
+ 
+
+    closegraph();
+
+    return 0;
+}
