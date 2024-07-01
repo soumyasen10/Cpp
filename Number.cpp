a78e58b16ab518ec05b10e1d4424dc10b28d564b
Soumya <111266105+soumyasen10@users.noreply.github.com>
2024-07-01 20:21:41 +0530

    Update Number.cpp

--- Number.cpp
+++ Number.cpp
@@ -9,7 +9,6 @@ int main()
         cout << number << " is a positive number." << endl; 
     } 
     else { 
-      
         cout << number << " is a negative number." << endl; 
     } 
     return 0; 
