bd1d5655ee96b6be59161ca7180ab90141bb9cde
Soumya <111266105+soumyasen10@users.noreply.github.com>
2024-07-04 14:32:47 +0530

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -4,8 +4,7 @@ using namespace std;
   
 int main() 
 { 
-    char ch = 'e'; 
-  
+    char ch = 'e';
     if (isalpha(ch)) { 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'
             || ch == 'u' || ch == 'A' || ch == 'E'
